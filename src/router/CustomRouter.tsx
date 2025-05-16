76d678d4c6670addecd6fe377ce572f89daa8098
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-16 18:01:02 +0200

    *add* front / Router

---
+++ src/router/CustomRouter.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "../App";
+import Page1 from "../pages/Page1";
+import Page2 from "../pages/Page2";
+
+function CustomRouter() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/page1" element={<Page1/>} />
+                <Route path="/page2" element={<Page2/>} />
+            </Routes>
+        </BrowserRouter>
+      );
+  }
+  
+  export default CustomRouter
