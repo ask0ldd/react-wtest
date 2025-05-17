4df8237113354295f48e2a3cd4932aef61d1cc18
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-17 20:47:54 +0200

    *edit* front / router updating

--- src/router/CustomRouter.tsx
+++ src/router/CustomRouter.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from "../App";
 import Page1 from "../pages/Page1";
 import Page2 from "../pages/Page2";
+import Home from "../pages/Home";
 
 function CustomRouter() {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/page1" element={<Page1/>} />
                 <Route path="/page2" element={<Page2/>} />
             </Routes>
