1ba25b482833b2d031f242c25efc66a546d73558
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-17 20:47:06 +0200

    *add* front / layout context

---
+++ src/context/layoutModeContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState, type ReactNode } from "react";
+
+export interface LayoutModeContextType {
+  layoutModeActive : boolean
+  switchLayoutMode : () => void
+}
+
+const defaultContextValue : LayoutModeContextType = {
+    layoutModeActive : false,
+    switchLayoutMode : () => void 0
+}
+
+export const LayoutModeContext = createContext<LayoutModeContextType>(defaultContextValue);
+
+interface LayoutModeProviderProps {
+  children: ReactNode;
+}
+
+export function LayoutModeProvider({ children }: LayoutModeProviderProps) {
+
+    const [layoutModeActive, setLayoutModeActive] = useState(false);
+    const switchLayoutMode = () => setLayoutModeActive((prev) => !prev);
+
+    const contextValue: LayoutModeContextType = {
+        layoutModeActive,
+        switchLayoutMode,
+    };
+
+    return (
+        <LayoutModeContext.Provider value={contextValue}>
+            {children}
+        </LayoutModeContext.Provider>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { ServicesProvider } from './context/services-context.tsx'
+import { ServicesProvider } from './context/servicesContext.tsx'
 import CustomRouter from './router/CustomRouter.tsx'
+import { LayoutModeProvider } from './context/layoutModeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ServicesProvider>
+    <ServicesProvider>
+      <LayoutModeProvider>
         <CustomRouter/>
-      </ServicesProvider>
+      </LayoutModeProvider>
+    </ServicesProvider>
   </StrictMode>,
 )
