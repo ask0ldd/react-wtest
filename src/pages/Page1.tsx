36215b7edcdc76f705148c25cf688558560ac841
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-17 20:47:38 +0200

    *edit* front / page 1 & 2 basic styling

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,7 +1,12 @@
+import Header from "../components/Header";
+
 export default function Page1(){
     return(
-        <div>
-            Page 1
+        <div className='flex flex-col w-full'>
+            <Header/>
+            <main className='flex flex-col w-full justify-center mt-[2rem]'>
+                Page 1
+            </main>
         </div>
     )
 }
--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,7 +1,12 @@
+import Header from "../components/Header";
+
 export default function Page2(){
     return(
-        <div>
-            Page 2
+        <div className='flex flex-col w-full'>
+            <Header/>
+            <main className='flex flex-col w-full justify-center mt-[2rem]'>
+                Page 2
+            </main>
         </div>
     )
 }
